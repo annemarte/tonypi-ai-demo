cb674782a24b0b915289a02531c6c8cfcf4864ff
annemarte <annemarte@jpro.no>
2026-01-04 17:32:17 +0100

    stubs og hiwonder libs

--- src/robot/controller.py
+++ src/robot/controller.py
@@ -1,8 +1,42 @@
+import time
+
+try:
+    # Real hardware (on TonyPi)
+    import hiwonder.ActionGroupControl as AGC
+    from hiwonder.Controller import Controller
+    import hiwonder.ros_robot_controller_sdk as rrc
+    import hiwonder.yaml_handle as yaml_handle
+    SIMULATION_MODE = False
+except ImportError:
+    # Stubs for local development
+    import stubs.ActionGroupControl as AGC
+    from stubs.Controller import Controller
+    import stubs.ros_robot_controller_sdk as rrc
+    import stubs.yaml_handle as yaml_handle
+    SIMULATION_MODE = True
+    print("[WARNING] Running in simulation mode - no real hardware")
 class TonyPiController:
+
+    last_cmd_time = 0
+    CMD_COOLDOWN = 1.5  # seconds
+
     def __init__(self, serial_port):
         self.serial_port = serial_port
+        board = rrc.Board()
+        ctl = Controller(board)
+
+        servo_data = yaml_handle.get_yaml_data(yaml_handle.servo_file_path)
+        ctl.set_pwm_servo_pulse(1, 1500, 500)
+        ctl.set_pwm_servo_pulse(2, servo_data['servo2'], 500)
+        AGC.runActionGroup('stand')
         pass
 
     def move(self, action: str):
+        now = time.time()
+        if self.last_cmd_time != 0 and now - self.last_cmd_time < self.CMD_COOLDOWN:
+            return
+        self.last_cmd_time = now
         print(f"[Robot] Executing action: {action}")
-        #todo implement the action
+        
+        AGC.runActionGroup(action, 2, True)
+        
