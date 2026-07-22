f2036cbd6b38790dadc420cf554b8cd3a7d64152
annemarte <annemarte@jpro.no>
2026-07-22 14:56:47 +0200

    Modularize AHT10 sensor logic into `temperature_sensor.py` and update `temperature_detection.py` to reuse it.

---
+++ src/robot/temperature_sensor.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+# @Author: Aiden
+# @Date: 2024/09/21
+import time
+import smbus
+
+
+class AHT10:
+    CONFIG = [0x08, 0x00]
+    MEASURE = [0x33, 0x00]
+
+    def __init__(self, bus=1, addr=0x38):
+        self.bus = smbus.SMBus(bus)
+        self.addr = addr
+        time.sleep(0.2)
+
+    def getData(self):
+        byte = self.bus.read_byte(self.addr)
+        self.bus.write_i2c_block_data(self.addr, 0xAC, self.MEASURE)
+        time.sleep(0.5)
+        data = self.bus.read_i2c_block_data(self.addr, 0x00)
+        temp = ((data[3] & 0x0F) << 16) | (data[4] << 8) | data[5]
+        ctemp = ((temp*200) / 1048576) - 50
+        hum = ((data[1] << 16) | (data[2] << 8) | data[3]) >> 4
+        chum = int(hum * 100 / 1048576)
+
+        return (ctemp, chum)
+
+
+def read_temperature_humidity(bus=1, addr=0x38):
+    """Read temperature (in Celsius) and humidity (in %) from the AHT10 sensor.
+
+    Returns:
+        tuple: (temperature, humidity)
+    """
+    aht10 = AHT10(bus=bus, addr=addr)
+    return aht10.getData()
+
+
+if __name__ == '__main__':
+    while True:
+        temperature, humidity = read_temperature_humidity()
+
+        print("Temperature: {:.1f} C, Humidity: {:.1f} %".format(temperature, humidity))
+
+        time.sleep(2)
